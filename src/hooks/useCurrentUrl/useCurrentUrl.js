55ffa9f85d91bab45369b26b61fb97401a04fab0
brandonsueur <gynidark@gmail.com>
2020-09-13 20:54:44 +0200

    feat: add hook useCurrentUrl

---
+++ src/hooks/useCurrentUrl/useCurrentUrl.js
@@ -0,0 +1,15 @@
+import { useLocation } from "react-router-dom";
+
+/**
+ * useCurrentUrl
+ */
+const useCurrentUrl = () => {
+  const { pathname, search } = useLocation();
+  return {
+    currentUrl: pathname + search,
+    pathname,
+    search,
+  };
+};
+
+export default useCurrentUrl;
