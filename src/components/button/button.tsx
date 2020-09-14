00d8aaa3a2e67bc70912c7a2012ed7522e5307a7
brandonsueur <gynidark@gmail.com>
2020-09-14 07:25:32 +0200

    feat: create component Button with TypeScript

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
+type ButtonProps = {
+  icon?: string;
+  label: string;
+  to?: string;
+  color?: string;
+  variant?: string;
+  loading?: boolean;
+};
+
 /**
  * Button
  * @param {string} icon
  * @param {string} label
+ * @param {string} to
  * @param {string} color - (success, primary, secondary, warning, error)
  * @param {string} variant - (contained, outlined, text)
  * @param {bool} loading
  */
-const Button = ({ icon, label, href, ...props }) => {
+const Button = ({ icon, label, to, ...props }: ButtonProps) => {
   return (
-    <a href={href} {...props}>
+    <a href={to} {...props}>
       {icon} {label}
     </a>
   );
