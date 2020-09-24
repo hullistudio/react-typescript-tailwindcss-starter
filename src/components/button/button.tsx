bacca66cbe2cef7d95dcc176ecbfac8789095e10
brandonsueur <gynidark@gmail.com>
2020-09-24 23:14:13 +0200

    feat: add props button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -6,7 +6,10 @@ type ButtonProps = {
   to?: string;
   color?: string;
   variant?: string;
+  size?: string;
+  fullWidth?: boolean;
   loading?: boolean;
+  disabled?: boolean;
 };
 
 /**
@@ -16,7 +19,10 @@ type ButtonProps = {
  * @param {string} to
  * @param {string} color - (success, primary, secondary, warning, error)
  * @param {string} variant - (contained, outlined, text)
+ * @param {string} size - (small, medium, large)
+ * @param {bool} fullWidth
  * @param {bool} loading
+ * @param {bool} disabled
  */
 const Button = ({ icon, label, to, ...props }: ButtonProps) => {
   return (
