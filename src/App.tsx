f7775358e259f34ace8f379b231777253135b5d8
brandonsueur <gynidark@gmail.com>
2020-09-26 20:09:58 +0200

    chore: add classnames and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from "react";
+import { Button } from "./components";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+        <Button label="Valider" />
+        <Button label="Valider" loading={true} />
+        <Button label="Valider" />
+        <Button label="Valider" fullWidth={true} />
       </header>
     </div>
   );
