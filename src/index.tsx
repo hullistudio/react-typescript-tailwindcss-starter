17ebc970c264efdcab68dd2478e29b66f4912f71
brandonsueur <gynidark@gmail.com>
2020-09-27 18:07:54 +0200

    chore: add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+import "./styles/index.css";
+
 import App from "./App";
 
 ReactDOM.render(
